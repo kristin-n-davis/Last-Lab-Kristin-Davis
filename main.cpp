92a48024ce0cc244e352601308e5cf459c9d6eed
kristin-n-davis <152207422+kristin-n-davis@users.noreply.github.com>
2023-12-02 21:02:43 -0600

    Update main.cpp
    creates the print menu function

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include "PlaylistNode.h"
 using namespace std;
+void PrintMenu(const string playlistTitle) {
+   cout << playlistTitle << " PLAYLIST MENU" << endl;
+   cout << "a - Add song" << endl;
+   cout << "d - Remove song" << endl;
+   cout << "c - Change position of song" << endl;
+   cout << "s - Output songs by specific artist" << endl;
+   cout << "t - Output total time of playlist (in seconds)" << endl;
+   cout << "o - Output full playlist" << endl;
+   cout << "q - Quit" << endl;
+   cout << endl;
+   
+   
+}
