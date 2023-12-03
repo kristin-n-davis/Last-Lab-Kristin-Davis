b86201324caf1c335c95b3d0dc7a2970d7ea235c
kristin-n-davis <152207422+kristin-n-davis@users.noreply.github.com>
2023-12-02 20:46:12 -0600

    Update PlaylistNode.cpp
    defines and initializes the variables in the parameterized constructor

--- PlaylistNode.cpp
+++ PlaylistNode.cpp
@@ -6,3 +6,10 @@ PlaylistNode::PlaylistNode(){
    songLength = 0;
    nextNodePtr = nullptr;
 }
+PlaylistNode::PlaylistNode(string songID, string songsName, string songArtist, int songsLength){
+   uniqueID = songID;
+   songName = songsName;
+   artistName = songArtist;
+   songLength = songsLength;
+   nextNodePtr = nullptr;
+}
